03cce4d39d81abffe497afeebb60468facd80f48
Hosea <hoseaabawono@gmail.com>
2022-07-06 00:46:06 +0700

    slicing login, checkout, succheck

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,60 +1,8 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('content')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('css/main.css')}}" type="text/css">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
-
-    <title>Laracamp by BuildWith Angga</title>
-</head>
-
-<body>
-
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="{{asset('images/logo.png')}}" alt="">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">My Guitar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Category</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Cart</a>
-                    </li>
-                </ul>
-                <div class="d-flex">
-                    <a href="#" class="btn btn-master btn-secondary me-3">
-                        Sign In
-                    </a>
-                    <a href="#" class="btn btn-master btn-primary">
-                        Sign Up
-                    </a>
-                </div>
-            </div>
-        </div>
-    </nav>
-
-    <section class="banner">
+<section class="banner">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-lg-11 col-12">
@@ -115,15 +63,15 @@
                         <div class="col-lg-6 col-12">
                             <div class="table-pricing paket-gila">
                                 <p class="new text-center">
-                                    GILA BELAJAR
+                                Gretsch G5420T
                                 </p>
                                 <h1 class="price text-center">
-                                    $280K
+                                    $800K
                                 </h1>
                                 <div class="item-benefit-pricing mb-4">
                                     <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
-                                        Pro Techstack Kit
+                                    Maple Wood
                                     </p>
                                     <div class="clear"></div>
                                     <div class="divider"></div>
@@ -131,7 +79,7 @@
                                 <div class="item-benefit-pricing mb-4">
                                     <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
-                                        iMac Pro 2021 & Display
+                                    Hollow Body
                                     </p>
                                     <div class="clear"></div>
                                     <div class="divider"></div>
@@ -139,7 +87,7 @@
                                 <div class="item-benefit-pricing mb-4">
                                     <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
-                                        1-1 Mentoring Program
+                                    22 Frets
                                     </p>
                                     <div class="clear"></div>
                                     <div class="divider"></div>
@@ -147,7 +95,7 @@
                                 <div class="item-benefit-pricing mb-4">
                                     <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
-                                        Final Project Certificate
+                                    Four Color
                                     </p>
                                     <div class="clear"></div>
                                     <div class="divider"></div>
@@ -155,7 +103,7 @@
                                 <div class="item-benefit-pricing mb-4">
                                     <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
-                                        Offline Course Videos
+                                    Sixth Strings
                                     </p>
                                     <div class="clear"></div>
                                     <div class="divider"></div>
@@ -163,29 +111,13 @@
                                 <div class="item-benefit-pricing mb-4">
                                     <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
-                                        Future Job Opportinity
+                                    Single Cutaway
                                     </p>
                                     <div class="clear"></div>
                                     <div class="divider"></div>
-                                </div>
-                                <div class="item-benefit-pricing mb-4">
-                                    <img src="{{asset('images/ic_check.svg')}}" alt="">
-                                    <p>
-                                        Premium Design Kit
-                                    </p>
-                                    <div class="clear"></div>
-                                    <div class="divider"></div>
-                                </div>
-                                <div class="item-benefit-pricing">
-                                    <img src="{{asset('images/ic_check.svg')}}" alt="">
-                                    <p>
-                                        Website Builder
-                                    </p>
-                                    <div class="clear"></div>
-                                </div>
-                                <p>
-                                    <a href="#" class="btn btn-master btn-primary w-100 mt-3">
-                                        Take This Plan
+                                </div>                            
+                                    <a href="{{route('checkout')}}" class="btn btn-master btn-primary w-100 mt-3">
+                                        Pick This Guitar
                                     </a>
                                 </p>
                             </div>
@@ -193,15 +125,15 @@
                         <div class="col-lg-6 col-12">
                             <div class="table-pricing paket-biasa">
                                 <p class="new text-center">
-                                    BARU MULAI
+                                Yamaha SLG200S String
                                 </p>
                                 <h1 class="price text-center">
-                                    $140K
+                                    $400K
                                 </h1>
                                 <div class="item-benefit-pricing mb-4">
                                     <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
-                                        1-1 Mentoring Program
+                                    Mahogany Wood
                                     </p>
                                     <div class="clear"></div>
                                     <div class="divider"></div>
@@ -209,7 +141,7 @@
                                 <div class="item-benefit-pricing mb-4">
                                     <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
-                                        Final Project Certificate
+                                    Four Natural Color
                                     </p>
                                     <div class="clear"></div>
                                     <div class="divider"></div>
@@ -217,21 +149,14 @@
                                 <div class="item-benefit-pricing mb-4">
                                     <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
-                                        Offline Course Videos
+                                    Silent String guitar
                                     </p>
                                     <div class="clear"></div>
                                     <div class="divider"></div>
-                                </div>
-                                <div class="item-benefit-pricing">
-                                    <img src="{{asset('images/ic_check.svg')}}" alt="">
-                                    <p>
-                                        Future Job Opportinity
-                                    </p>
-                                    <div class="clear"></div>
-                                </div>
+                                </div>                            
                                 <p>
-                                    <a href="#" class="btn btn-master btn-secondary w-100 mt-3">
-                                        Start With This Plan
+                                    <a href="{{route('checkout')}}" class="btn btn-master btn-secondary w-100 mt-3">
+                                        First Buy
                                     </a>
                                 </p>
                             </div>
@@ -332,11 +257,4 @@
         </div>
     </section>
 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-
-</body>
-
-</html>
+@endsection
